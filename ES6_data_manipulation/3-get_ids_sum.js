b2a8e31b2dbbdc4321cb959bf3df4e76708c2cf0
Maxime521 <9842@holbertonstudents.com>
2025-03-31 22:53:19 +0200

    disable eslint

--- ES6_data_manipulation/3-get_ids_sum.js
+++ ES6_data_manipulation/3-get_ids_sum.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function getStudentIdsSum(students) {
     return students.reduce((add, student) => add + student.id, 0);
   }
--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,3 +1,4 @@
+/* eslint-disable */
 export default function createInt8TypedArray(length, position, value) {
     const buffer = new ArrayBuffer(length);
     const int8 = new Int8Array(buffer);
