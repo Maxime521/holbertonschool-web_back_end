541b1d66ffc0c2a55761fd7c5aa27f6f77acdea1
Maxime521 <9842@holbertonstudents.com>
2025-04-03 09:44:41 +0200

    I just reorganized the code

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -2,13 +2,8 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.reason || result.value,
-    })));
-}
+export default (firstName, lastName, fileName) => Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+  .then((results) => results.map((promise) => ({
+    status: promise.status,
+    value: promise.status === 'fulfilled' ? promise.value : `${promise.reason.name}: ${promise.reason.message}`,
+})));
