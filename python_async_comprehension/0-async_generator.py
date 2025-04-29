e15de910fe25a4d8fa30c73bfc604c1532902038
Maxime521 <9842@holbertonstudents.com>
2025-04-29 10:56:47 +0200

    I just reorganized the task 0 and i start the task 1 and 2

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
-
-"""Module for async_generator function"""
-
+"""
+Module for async_generator function
+"""
 import asyncio
 import random
+from typing import Generator
 
 
-async def async_generator():
+async def async_generator() -> Generator[float, None, None]:
     """
     An asynchronous generator that yields a random number between 0 and 10
     after asynchronously waiting for 1 second, 10 times.
