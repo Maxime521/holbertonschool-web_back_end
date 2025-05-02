cd564d5c4db96e0f49a596fe3116b9fba16a001b
Maxime521 <9842@holbertonstudents.com>
2025-05-02 21:41:19 +0200

    I just reorganized the code pour my pycodestyle

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -1,23 +1,26 @@
 #!/usr/bin/env python3
+"""Module for log_stats function"""
 from pymongo import MongoClient
 
 
 def log_stats():
-    """Provides some stats about Nginx logs stored in MongoDB."""
-    client = MongoClient()
+    """provides some stats about Nginx logs stored in MongoDB"""
+    client = MongoClient('localhost', 27017)
     db = client.logs
     collection = db.nginx
 
-    total_logs = collection.count_documents({})
-    print(f"{total_logs} logs")
-
-    print("Methods:")
+    logs_qty = collection.count_documents({})
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    for method in methods:
-        count = collection.count_documents({"method": method})
-        print(f"\tmethod {method}: {count}")
+    status_check = collection.count_documents({
+        "method": "GET",
+        "path": "/status"
+        })
 
-    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    print(f"{logs_qty} logs")
+    print("Methods:")
+    for method in methods:
+        method_qty = collection.count_documents({"method": method})
+        print(f"\tmethod {method}: {method_qty}")
     print(f"{status_check} status check")
 
 
