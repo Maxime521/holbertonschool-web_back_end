73f95294035dfd0743138bec7823b499e088034c
Maxime521 <9842@holbertonstudents.com>
2026-02-23 18:56:19 +0100

    feat: Add RedactingFormatter class for log redaction with configurable fields

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ Module for filtering and obfuscating log messages containing PII
 """
 import re
 from typing import List
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str,
@@ -27,3 +28,22 @@ def filter_datum(fields: List[str], redaction: str,
             f'{field}={redaction}{separator}',
             message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+    """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = list(fields)
+
+    def format(self, record: logging.LogRecord) -> str:
+        message = record.getMessage()
+        record.msg = filter_datum(
+            self.fields, self.REDACTION, message, self.SEPARATOR)
+        return super().format(record)
