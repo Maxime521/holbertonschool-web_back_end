44daa19fd2fcfaafc06b20cc8a105d177755b16f
Maxime521 <9842@holbertonstudents.com>
2026-02-23 14:58:26 +0100

    I just adjusted all the spacing and shortened the sentences.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -3,23 +3,27 @@
 Module for filtering and obfuscating log messages containing PII
 """
 
+
 import re
 from typing import List
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """
-        Returns the log message obfuscated by replacing specified field values
-        with a redaction string.
+        Returns log message with specified fields redacted.
 
         Args:
             fields: list of strings representing fields to obfuscate
             redaction: string to replace field values with
-            message: string representing the log line
-            separator: character separating fields in the log line
+            message: the log line
+            separator: character separating fields
 
         Returns:
-        The obfuscated log message
+            The obfuscated log message
     """
     for field in fields:
-        message = re.sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+        message = re.sub(
+            f'{field}=.*?{separator}',
+            f'{field}={redaction}{separator}', message)
         return message
