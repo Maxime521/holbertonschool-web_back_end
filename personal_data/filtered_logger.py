001cd56e26209687dac462f2528090d1f31be28e
Maxime521 <9842@holbertonstudents.com>
2026-02-23 14:53:26 +0100

    Add filtered_logger module: Implement a logger that filters and obfuscates log messages containing personally identifiable information (PII).

---
+++ personal_data/filtered_logger.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+"""
+Module for filtering and obfuscating log messages containing PII
+"""
+
+import re
+from typing import List
+
+def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+    """
+        Returns the log message obfuscated by replacing specified field values
+        with a redaction string.
+
+        Args:
+            fields: list of strings representing fields to obfuscate
+            redaction: string to replace field values with
+            message: string representing the log line
+            separator: character separating fields in the log line
+
+        Returns:
+        The obfuscated log message
+    """
+    for field in fields:
+        message = re.sub(f'{field}=.*?{separator}', f'{field}={redaction}{separator}', message)
+        return message
