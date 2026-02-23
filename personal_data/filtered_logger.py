3e284c84f5242331eee8e857effa212772dffb6b
Maxime521 <9842@holbertonstudents.com>
2026-02-23 14:59:56 +0100

    I've reorganized everything.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,9 +1,7 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 """
 Module for filtering and obfuscating log messages containing PII
 """
-
-
 import re
 from typing import List
 
@@ -11,19 +9,21 @@ from typing import List
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """
-        Returns log message with specified fields redacted.
+    Returns the log message obfuscated by replacing specified field values
+    with a redaction string.
 
-        Args:
-            fields: list of strings representing fields to obfuscate
-            redaction: string to replace field values with
-            message: the log line
-            separator: character separating fields
+    Args:
+        fields: list of strings representing fields to obfuscate
+        redaction: string to replace field values with
+        message: string representing the log line
+        separator: character separating fields in the log line
 
-        Returns:
-            The obfuscated log message
+    Returns:
+        The obfuscated log message
     """
     for field in fields:
         message = re.sub(
             f'{field}=.*?{separator}',
-            f'{field}={redaction}{separator}', message)
-        return message
+            f'{field}={redaction}{separator}',
+            message)
+    return message
