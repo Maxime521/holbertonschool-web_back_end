b3c61b0578459ded04733a0ae5656b82a417f961
Maxime521 <9842@holbertonstudents.com>
2026-02-24 01:08:32 +0100

    Implement hash_password function using bcrypt and install the packages bcrypt

---
+++ personal_data/encrypt_password.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""
+Module for encrypting passwords
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> bytes:
+    """
+    Hash a password using bcrypt
+    """
+    return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
