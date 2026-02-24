cd68d8c7f13b87443cdc8ae50f57e00f16593b11
Maxime521 <9842@holbertonstudents.com>
2026-02-24 01:12:07 +0100

    I just finished the project

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -11,3 +11,10 @@ def hash_password(password: str) -> bytes:
     Hash a password using bcrypt
     """
     return bcrypt.hashpw(password.encode(), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Check if a password is valid
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
