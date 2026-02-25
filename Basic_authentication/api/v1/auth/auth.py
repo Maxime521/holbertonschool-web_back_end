34c65f34a966a790ae88c88b8c4b7890eb045bd7
Maxime521 <9842@holbertonstudents.com>
2026-02-25 14:46:17 +0100

    feat: Add Auth class template with require_auth, authorization_header and current_user methods

---
+++ Basic_authentication/api/v1/auth/auth.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+""" Module of Auth views
+"""
+
+from typing import List, TypeVar
+from flask import request
+
+
+class Auth:
+    """ Auth class
+    """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """Check if authentication is required"""
+        return False
+    
+    def authorization_header(self, request=None) -> str:
+        """Get authorization header from request
+        """
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """Get current user from request"""
+        return None
