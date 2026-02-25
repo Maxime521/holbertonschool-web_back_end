95c752a04eba04d6e7990a6fac77d825bb41826d
Maxime521 <9842@holbertonstudents.com>
2026-02-25 23:56:05 +0100

    remove line

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -14,8 +14,8 @@ app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth_type = getenv("AUTH_TYPE")
-if auth_type == "auth":
-    from api.v1.auth.auth import BasicAuth
+if auth_type == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
 else:
     from api.v1.auth.auth import Auth
