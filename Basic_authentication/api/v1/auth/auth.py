59060503e3cd642f840f8cfad0c7fd73f883b6e4
Maxime521 <9842@holbertonstudents.com>
2026-02-25 23:54:19 +0100

    feat: Add BasicAuth class inheriting from Auth and update app.py to use it based on AUTH_TYPE env var

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -28,7 +28,13 @@ class Auth:
     def authorization_header(self, request=None) -> str:
         """Get authorization header from request
         """
-        return None
+        if request is None:
+            return None
+        
+        if request.headers.get('Authorization') is None:
+            return None
+        
+        return request.headers.get('Authorization')
 
     def current_user(self, request=None) -> TypeVar('User'):
         """Get current user from request"""
