37791f57b6e1eb6d57797f8f8148ceb331b1b324
Maxime521 <9842@holbertonstudents.com>
2026-02-26 13:36:39 +0100

    reorganized the code

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -9,17 +9,17 @@ class BasicAuth(Auth):
     """
     pass
 
-def extract_base64_authorization_header(
-        self, authorization_header: str) -> str:
-    """ Extract base64 authorization header"""
+    def extract_base64_authorization_header(
+            self, authorization_header: str) -> str:
+        """ Extract base64 authorization header"""
 
-    if authorization_header is None:
-        return None
-    
-    if not isinstance(authorization_header, str):
-        return None
-    
-    if not authorization_header.startswith("Basic "):
-        return None
-    
-    return authorization_header.split(" ")[1]
+        if authorization_header is None:
+            return None
+
+        if not isinstance(authorization_header, str):
+            return None
+
+        if not authorization_header.startswith("Basic "):
+            return None
+
+        return authorization_header.split(" ")[1]
