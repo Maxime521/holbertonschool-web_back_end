4bd32262e7b5d7dd6d47e234cf5d5c75a4b0078d
Maxime521 <9842@holbertonstudents.com>
2026-02-26 23:11:21 +0100

    Fix: pycodestyle in files basic_auth

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -7,6 +7,7 @@ from typing import Tuple, TypeVar
 from api.v1.auth.auth import Auth
 from models.user import User
 
+
 class BasicAuth(Auth):
     """ BasicAuth class
     """
@@ -65,22 +66,21 @@ class BasicAuth(Auth):
 
         if user_email is None or not isinstance(user_email, str):
             return None
-        
+
         if user_pwd is None or not isinstance(user_pwd, str):
             return None
-        
+
         try:
             users = User.search({"email": user_email})
         except Exception:
             return None
-        
+
         if not users or len(users) == 0:
             return None
-        
+
         user = users[0]
 
         if not user.is_valid_password(user_pwd):
             return None
-        
+
         return user
-    
