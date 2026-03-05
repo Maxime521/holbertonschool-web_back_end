0cd44d5aa861405c4cf82d6eca6a8e4921bd342f
Maxime521 <9842@holbertonstudents.com>
2026-03-05 22:28:47 +0100

    update the files

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -11,39 +11,35 @@ class SessionAuth(Auth):
     """Session authentication class that inherits from Auth
     """
 
+    user_id_by_session_id = {}
 
-user_id_by_session_id = {}
+    def create_session(self, user_id: str = None) -> str:
+        """Create a Session ID for a given user_id
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
 
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
 
-def create_session(self, user_id: str = None) -> str:
-    """Create a Session ID for a given user_id
-    """
-    if user_id is None or not isinstance(user_id, str):
-        return None
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """Return a User ID based on a Session ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
 
-    session_id = str(uuid4())
-    self.user_id_by_session_id[session_id] = user_id
-    return session_id
+        return self.user_id_by_session_id.get(session_id)
 
+    def current_user(self, request=None):
+        """Return a User instance based on a cookie value
+        """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
 
-def user_id_for_session_id(self, session_id: str = None) -> str:
-    """Return a User ID based on a Session ID
-    """
-    if session_id is None or not isinstance(session_id, str):
-        return None
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
 
-    return self.user_id_by_session_id.get(session_id)
-
-
-def current_user(self, request=None):
-    """Return a User instance based on a cookie value
-    """
-    session_id = self.session_cookie(request)
-    if session_id is None:
-        return None
-
-    user_id = self.user_id_for_session_id(session_id)
-    if user_id is None:
-        return None
-
-    return User.get(user_id)
+        return User.get(user_id)
