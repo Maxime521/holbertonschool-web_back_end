ee02b627067f5674b715c30f5ee5e126c5741330
Maxime521 <9842@holbertonstudents.com>
2026-03-05 22:20:37 +0100

    update the pycodestyle

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -14,15 +14,17 @@ class SessionAuth(Auth):
 
 user_id_by_session_id = {}
 
+
 def create_session(self, user_id: str = None) -> str:
     """Create a Session ID for a given user_id
     """
     if user_id is None or not isinstance(user_id, str):
         return None
-    
+
     session_id = str(uuid4())
     self.user_id_by_session_id[session_id] = user_id
     return session_id
+
 
 def user_id_for_session_id(self, session_id: str = None) -> str:
     """Return a User ID based on a Session ID
@@ -31,6 +33,7 @@ def user_id_for_session_id(self, session_id: str = None) -> str:
         return None
 
     return self.user_id_by_session_id.get(session_id)
+
 
 def current_user(self, request=None):
     """Return a User instance based on a cookie value
